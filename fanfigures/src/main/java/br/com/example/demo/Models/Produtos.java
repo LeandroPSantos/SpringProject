400dddaebdbd7938060dc22b88e8e2c61c55ae21
Leand <Leand@LAPTOP-RMPOAMEL.mshome.net>
2020-03-06 21:59:18 -0300

    feat: implementação conexão com banco e migration da nova classe produto

---
+++ fanfigures/src/main/java/br/com/example/demo/Models/Produtos.java
@@ -0,0 +1,132 @@
+package br.com.example.demo.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produtos {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id_produto;
+
+	private int cod_fornecedor;
+
+	private String nome;
+
+	private String descricao;
+
+	private double preco;
+
+	private int estoque;
+
+	private String link_img;
+
+	private int tamanho;
+
+	private int peso;
+
+	private String material;
+
+	private String fabricante;
+
+	private String codigo_serie;
+
+	public int getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(int tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	public String getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public String getCodigo_serie() {
+		return codigo_serie;
+	}
+
+	public void setCodigo_serie(String codigo_serie) {
+		this.codigo_serie = codigo_serie;
+	}
+
+	public int getId_produto() {
+		return id_produto;
+	}
+
+	public void setId_produto(int id_produto) {
+		this.id_produto = id_produto;
+	}
+
+	public int getCod_fornecedor() {
+		return cod_fornecedor;
+	}
+
+	public void setCod_fornecedor(int cod_fornecedor) {
+		this.cod_fornecedor = cod_fornecedor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(int estoque) {
+		this.estoque = estoque;
+	}
+
+	public String getLink_img() {
+		return link_img;
+	}
+
+	public void setLink_img(String link_img) {
+		this.link_img = link_img;
+	}
+}
